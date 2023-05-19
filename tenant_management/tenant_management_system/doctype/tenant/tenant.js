6ff93a68bac7aaf0d304c62c61a24323ef8d757d
kelue <austincmoon5@gmail.com>
2023-05-19 09:31:07 +0100

    added form script for tenant doctype

--- tenant_management/tenant_management_system/doctype/tenant/tenant.js
+++ tenant_management/tenant_management_system/doctype/tenant/tenant.js
@@ -1,8 +1,17 @@
 // Copyright (c) 2023, Nzeamalu Chikelue and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Tenant", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on('Tenant', {
+    refresh: function(frm) {
+        frm.add_custom_button('Create Rental Agreement', () => {
+            frappe.new_doc('Rental Agreement', {
+                occupant: frm.doc.name
+            })
+        })
+        frm.add_custom_button('Create Rent Billing', () => {
+            frappe.new_doc('Rent Billing', {
+                occupant: frm.doc.name
+            })
+        })
+    }
+});
